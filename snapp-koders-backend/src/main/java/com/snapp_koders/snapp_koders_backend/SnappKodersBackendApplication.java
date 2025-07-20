1a960206b2e030d43d236205a4b56a39fbb21c5b
Pratik-Sarkate <pratiksarkate2242@gmail.com>
2025-07-20 18:32:30 +0530

    initial project setup

---
+++ snapp-koders-backend/src/main/java/com/snapp_koders/snapp_koders_backend/SnappKodersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.snapp_koders.snapp_koders_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnappKodersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnappKodersBackendApplication.class, args);
+	}
+
+}
---
+++ snapp-koders-backend/src/test/java/com/snapp_koders/snapp_koders_backend/SnappKodersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snapp_koders.snapp_koders_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnappKodersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
