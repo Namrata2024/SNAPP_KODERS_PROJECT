143869037f9b6236c45027328f69ba4aeeb3a95a
srushti-khatri0809 <srushtikhatri0809@gmail.com>
2025-07-23 01:18:23 +0530

    CORS issue fixed

--- nodejs-backend/index.js
+++ nodejs-backend/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyparser = require("body-parser");
+const cors = require("cors");
 
 const { PORT } = require("./config/serverConfig.js");
 const ApiRoutes = require("./routes/index");
@@ -7,6 +8,7 @@ const ApiRoutes = require("./routes/index");
 const setupAndStartServer = async () => {
   // create the express object
   const app = express();
+  app.use(cors());
   app.use(bodyparser.json());
 
   app.use("/api", ApiRoutes);
