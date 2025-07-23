eebb1121875b5ce7514b4bf81b579263422449f9
SrushtiHaryan <srushant23@gmail.com>
2025-07-23 21:06:10 +0530

    resolved conflicts

--- snapp-koders-frontend/src/App.jsx
+++ snapp-koders-frontend/src/App.jsx
@@ -1,42 +1,36 @@
-import React, { useState } from 'react';
-import SpeechLanguageSelector from './components/SpeechLanguageSelector';
-import SpeechToText from './components/SpeechToText';
-import FinancialAdvice from './components/FinancialAdvice';
-import ExpenseList from './components/ExpenseList';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { CssBaseline } from "@mui/material";
+
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
+import SpeechLanguageSelector from "./components/SpeechLanguageSelector";
+import SpeechToText from "./components/SpeechToText";
+import FinancialAdvice from "./components/FinancialAdvice";
+import ExpenseList from "./components/ExpenseList";
 
 const App = () => {
   const [selectedLang, setSelectedLang] = useState(null);
 
-// App.jsx
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import { CssBaseline } from "@mui/material";
-import SpeechToText from "./SpeechToText"
-import Home from "./pages/Home"
-
-function App() {
   return (
-    <div className="w-screen h-screen">
-        {!selectedLang ? (
-        <SpeechLanguageSelector onLanguageSelected={setSelectedLang} />
-      ) : (
-        <>
-          <SpeechToText selectedLang={selectedLang} />
-          <ExpenseList/>
-          <FinancialAdvice />
-        </>
-      )}
-  
-    </div>   
     <>
       <CssBaseline />
       <Router>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/speech-to-text" element={<SpeechToText />} />
+          <Route path="/speech-to-text" element={
+            !selectedLang ? (
+              <SpeechLanguageSelector onLanguageSelected={setSelectedLang} />
+            ) : (
+              <div className="w-screen h-screen">
+                <SpeechToText selectedLang={selectedLang} />
+                <ExpenseList />
+                <FinancialAdvice />
+              </div>
+            )
+          } />
           <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
